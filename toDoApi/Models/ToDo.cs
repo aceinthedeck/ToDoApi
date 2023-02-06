5a73dee21d439260cbbe4757614de27a810f56fc
Kay <kay@Kanikas-MacBook-Pro.local>
2023-02-06 16:42:15 -0500

    first

---
+++ toDoApi/Models/ToDo.cs
@@ -0,0 +1,13 @@
+using System;
+using toDoApi;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace toDoApi.Models
+{
+	public class ToDo
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+	}
+}
+
